a427db7f171371d743413b75f58afaf46240caac
Max W Schwarz <maxs@darkhorse.com>
2014-01-06 13:56:11 -0800

    Remove unnecesary and buggy name parameter
    This is the namevar, so it will default to the title of the resource in most
    cases. However, when $package_name is an array, it will create a separate
    resource for each element, while still passing in the entire array. Supplying an
    array for the name paramater was deprecated in Puppet 3.4.0, causing the catalog
    to fail when run on an Ubuntu system.

--- manifests/generic_service.pp
+++ manifests/generic_service.pp
@@ -37,9 +37,8 @@ define nova::generic_service(
   # I need to mark that ths package should be
   # installed before nova_config
   if ($package_name) {
-    package { $nova_title:
+    package { $package_name:
       ensure => $ensure_package,
-      name   => $package_name,
       notify => Service[$nova_title],
     }
   }
@@ -50,7 +49,7 @@ define nova::generic_service(
       name      => $service_name,
       enable    => $enabled,
       hasstatus => true,
-      require   => [Package['nova-common'], Package[$nova_title]],
+      require   => [Package['nova-common'], Package[$package_name]],
     }
   }
 
